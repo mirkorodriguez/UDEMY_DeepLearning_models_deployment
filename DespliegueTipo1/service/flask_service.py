a264f80aa311ae3e8d5c4276e25ce3e6421efbc0
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2020-07-02 14:53:39 -0500

    Updating scripts

--- DespliegueTipo1/service/flask_service.py
+++ DespliegueTipo1/service/flask_service.py
@@ -72,7 +72,7 @@ def predict(model_name):
             print("\nfilename:",filename)
 
             image_to_predict = tf.keras.preprocessing.image.load_img(filename, target_size=(224, 224))
-            test_image = image.img_to_array(image_to_predict)
+            test_image = tf.keras.preprocessing.image.img_to_array(image_to_predict)
             test_image = np.expand_dims(test_image, axis = 0)
             test_image = test_image.astype('float32')
             test_image /= 255
