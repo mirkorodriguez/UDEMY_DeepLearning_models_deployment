4c3c31b5269d6638283b91ea959f51a0282f39ec
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2020-07-06 14:01:27 -0500

    Updating Flask service scripts.

--- DespliegueTipo1/service/model_loader.py
+++ DespliegueTipo1/service/model_loader.py
@@ -8,11 +8,10 @@ import tensorflow as tf
 def cargarModeloH5():
 
     MODEL_H5_FILE = "flowers_model_tl.h5"
-    MODEL_PATH = "../../../models/keras/"
+    MODEL_H5_PATH = "../../../models/keras/"
 
     # Cargar el modelo DL desde disco
-    loaded_model = tf.keras.models.load_model(MODEL_PATH + MODEL_H5_FILE)
+    loaded_model = tf.keras.models.load_model(MODEL_H5_PATH + MODEL_H5_FILE)
     print(MODEL_H5_FILE, " cargado de disco >> ", loaded_model)
 
-    # graph = tf.get_default_graph()
     return loaded_model
